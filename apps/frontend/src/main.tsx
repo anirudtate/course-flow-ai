7d9fed67b0005baf380be05426f15b407975dbb1
anirudtate <anirud.programming@gmail.com>
2024-10-31 18:51:30 +0530

    create basic landing page structure

---
+++ apps/frontend/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "@/pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>,
+);
